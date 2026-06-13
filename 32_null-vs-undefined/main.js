f6bc2edcb89b39cae523d653d8324c15e081ccb0
N-Dave-08 <cdave.dev@gmail.com>
2026-06-13 20:13:45 +0800

    feat: null vs undefined ✅

---
+++ 32_null-vs-undefined/main.js
@@ -0,0 +1,14 @@
+console.log(null == undefined);
+// true
+console.log(null === undefined);
+// false
+
+// null is intentionally empty
+let a = null;
+console.log(a);
+// prints: null
+
+// undefined means the value is not assigned yet
+let b;
+console.log(b);
+// prints: undefined
